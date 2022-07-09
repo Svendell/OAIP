95219e97e66ae102456c5333e4b5cbde109893c0
Ilya <66868765+Svendell@users.noreply.github.com>
2022-07-09 14:10:15 +0300

    Create dop_4-1.cpp

---
+++ Laba_15/dop_4-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+float integ(float(*) (float), float, float, float);
+float f(float);
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	float z, a, b, h;
+	cout << "предел: a " << endl;
+	cin >> a;
+	cout << "предел: b " << endl;
+	cin >> b;
+	cout << "точность:" << endl;
+	cin >> h;
+	z = integ(f, (float)a, (float)b, (float)h);
+	std::cout << "Integral=" << z;
+}
+float integ(float(*f) (float), float a, float b, float h)
+{
+	float x, s = 0.0;
+	x = a + h;
+	while (x <= b)
+	{
+		s += h * f(x);
+		x = x + h;
+	}
+	return s / 2;
+}
+float f(float x)
+{
+	return (pow(2.7182818, x) - (1 / x) - 3);
+}
